875eba75f5f390925e947431f4a7ebd00671a103
tee1505 <mon.motoo01@gmail.com>
2025-11-20 09:41:54 +0700

    Create D1.php

---
+++ D1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"
+    <title>Document</title>
+    <head>
+    <body>
+        <?php
+        echo "Hello World!";
+        echo "<br>";
+        echo "<h2>My Name is John Doe</h2>";
+        ?> 
+    </body>
+</head>
+</html>
