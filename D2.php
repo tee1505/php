01d7837deab0a5320790121a30f4c6f6f9dc2687
tee1505 <mon.motoo01@gmail.com>
2025-12-04 09:32:18 +0700

    Update D2.php

--- D2.php
+++ D2.php
@@ -14,10 +14,20 @@
     <hr> <h1> ทอสอบ Array แบบ Associative Array </h1>
     <?php
     $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43" , "Mon"=>"25");
-    echo "Peter is " . $age['Peter'] . " years old." . "<br>"; 
-    echo "Ben is " . $age['Ben'] . " years old." . "<br>"; 
-    echo "Joe is " . $age['Joe'] . " years old." . "<br>";
-    echo "Joe is " . $age['Mon'] . " years old." . "<br>";
+    echo "Peter is " . $age['Peter'] . " years old." . "<br>\n"; 
+    echo "Ben is " . $age['Ben'] . " years old." . "<br>\n"; 
+    echo "Joe is " . $age['Joe'] . " years old." . "<br>\n";
+    echo "Joe is " . $age['Mon'] . " years old." . "<br>\n";
+    ?>
+    <h1>การใช้งาน For กับ Index Arrays </h1>
+    <?php
+    $fruits = array("แอปเปิ้ล", "มะละกอ", "กล้วย", "ส้ม" , "องุ่น","มังคุด","ทุเรียน");
+    $arrlength = count($fruits);
+
+    for($x = 0; $x < $arrlength; $x++) {
+        echo $fruits[$x];
+        echo "<br>\n";
+    }
     ?>
 </body>
 </html>
