3befb67bc3f6cdc04914441823eec03f717ea9a9
tee1505 <mon.motoo01@gmail.com>
2025-12-04 10:56:51 +0700

    Create PHPREQUEST.php

---
+++ PHPREQUEST.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <title>PHP Form Request</title>
+<head>
+<form method="post" action=" <?php echo $_SERVER['PHP_SELF' ]; ?>">
+    Name: <input type="text" name="fname"><br>
+    Last Name: <input type="text" name="lname"><br>
+    <input type="submit">
+</form>
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_REQUEST['fname' ];
+        $lname = $_REQUEST['lname' ];
+        if (empty($name)) {
+            echo "Name is empty";
+            } else {
+                echo $name;" " . $lname;
+
+            }
+
+    }
+
+?>
+
+</body>
+</html>
