da46657d86c70a3933b4a24eb49640ccf78bc3c1
tee1505 <mon.motoo01@gmail.com>
2025-12-04 09:18:34 +0700

    Update D2.php

--- D2.php
+++ D2.php
@@ -5,8 +5,19 @@
     <title>Php Array (เรียนวันที่ 4/12/2568)</title>
 </head>
 <body>
+    <h1> ทอสอบ Array แบบ Indexed Array </h1>
     <?php
+    $cars = array("Volvo", "BMW", "Toyota");
+    echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+    ?>
     
+    <hr> <h1> ทอสอบ Array แบบ Associative Array </h1>
+    <?php
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43" , "Mon"=>"25");
+    echo "Peter is " . $age['Peter'] . " years old." . "<br>"; 
+    echo "Ben is " . $age['Ben'] . " years old." . "<br>"; 
+    echo "Joe is " . $age['Joe'] . " years old." . "<br>";
+    echo "Joe is " . $age['Mon'] . " years old." . "<br>";
     ?>
 </body>
 </html>
