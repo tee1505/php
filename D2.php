02bed9010645e84408b206d55d33fb1f559b2898
tee1505 <mon.motoo01@gmail.com>
2025-12-04 08:59:38 +0700

    Create D2.php

---
+++ D2.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Php Array (เรียนวันที่ 4/12/2568)</title>
+</head>
+<body>
+    <?php
+    
+    ?>
+</body>
+</html>
